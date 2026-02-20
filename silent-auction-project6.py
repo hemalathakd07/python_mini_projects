df0bcca42fc764c27c79864cb0dba4fe554c4e55
hemalathakd07 <hemalathakd07@gmail.com>
2026-02-20 12:01:23 +0530

    removed extra lines

--- silent-auction-project6.py
+++ silent-auction-project6.py
@@ -10,14 +10,12 @@ def bid_winner(bidder_details):
     
     print(f"The bid winner is {winner} with the highest bid price of {highest_bid_price}")
 
-
 flag=False
 bidder_details={}
 while not flag:
     name=input("Enter the name of the bidder:")
     price=int(input("Enter the bid price:"))
     bidder_details[name]=price
-
     next_bidder=input("Is there any other bidder:(enter yes/no)")
     if next_bidder=="no":
         flag=True
