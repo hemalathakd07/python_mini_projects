6d33d4cf1bf80e1423f544676368c96ccfd7dc1f
hemalathakd07 <hemalathakd07@gmail.com>
2026-02-22 10:42:00 +0530

    added database of project 9

---
+++ game_database.py
@@ -0,0 +1,67 @@
+data= [
+    {
+        'name':"Jenny's lectures",
+        'follower_count':1,
+        'description':"youtuber",
+        'country':"india"
+    },
+    {
+        'name':"Cristiano Ronaldo",
+        'follower_count':600,
+        'description':"Footballer",
+        'country':"Portugual"
+    },
+    {
+        'name':"Narendra Modi",
+        'follower_count':90,
+        'description':"Prime minister",
+        'country':"india"
+    },
+    {
+         'name':"fit tuber",
+        'follower_count':7,
+        'description':"youtuber",
+        'country':"india"
+    },
+    {
+        'name':"selena gomez",
+        'follower_count':421,
+        'description':"musician and actress",
+        'country':"United states of america"
+    },
+    {
+         'name':"Kylie Jenner",
+        'follower_count':400,
+        'description':"Reality TV personalisation",
+        'country':"United states of america"
+    },
+    {
+        'name':"shakira",
+        'follower_count':87,
+        'description':"musician",
+        'country':"columbia"
+    },
+    {
+        'name':"ISRO",
+        'follower_count':1,
+        'description':"space agency",
+        'country':"india"
+    },
+    {
+        'name':"NASA",
+        'follower_count':94,
+        'description':"space agency",
+        'country':"Unites states"
+    },
+    {
+        'name':"virat kohli",
+        'follower_count':254,
+        'description':"cricketer",
+        'country':"india"
+    },{
+        'name':"neeraj chopra",
+        'follower_count':100,
+        'description':"athlete",
+        'country':"india"
+    }
+]
